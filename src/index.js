1bb2eb327a008c8070b6b0dd537c85059976ef89
Jose Obed Moguel Ruiz <jo_obed@hotmail.com>
2019-01-31 23:42:39 -0600

    Se agregan las librerias necesarias para poder configurar el manejador de estado 'redux', se configura la estructura del primer modulo main

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import main from './pages/main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    main.reducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/main/Container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Container extends Component {
+    render(){
+        return (
+            <div>
+                Hola yo soy el container
+            </div>
+        );
+    };
+};
+
+export default Container;
