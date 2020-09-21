96dc92976951b01b740f576d240f2c644ebed77f
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-09-21 16:02:28 -0500

    Updates

---
+++ precache-manifest.83c664d60cfbf70cbd98aa1c2ae0713d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeccfac230955964df02",
+    "url": "/react-venados-test/static/css/main.734169c2.chunk.css"
+  },
+  {
+    "revision": "eeccfac230955964df02",
+    "url": "/react-venados-test/static/js/main.eeccfac2.chunk.js"
+  },
+  {
+    "revision": "654dde3267a7833ee966",
+    "url": "/react-venados-test/static/js/1.654dde32.chunk.js"
+  },
+  {
+    "revision": "2a6f90f97a0d342e2e98",
+    "url": "/react-venados-test/static/js/runtime~main.2a6f90f9.js"
+  },
+  {
+    "revision": "d3301ff256a28003206a7d573452b3e1",
+    "url": "/react-venados-test/index.html"
+  }
+];
